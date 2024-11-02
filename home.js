1d004ca7ed033cc16c20f7305ed3f10c95390094
Shotaro-Ueno <saramidayo43@gmail.com>
2024-11-02 23:32:38 +0900

    newの作成とhome画面のアニメーション調整

--- home.js
+++ home.js
@@ -1,8 +1,19 @@
 window.addEventListener('DOMContentLoaded',function(){
-  const welcomes = document.querySelectorAll('.welcome-text');
-  welcomes.forEach(function(welcome,index){
+  const homes = document.querySelectorAll('.home');
+  homes.forEach(function(home,index){
     setTimeout(() => {
-      welcome.classList.add('is-active');
+      home.classList.add('is-active');
     },index*1600)
   });
 });
+
+
+setTimeout(secondact,2000);
+function secondact(){
+  const welcomes = document.querySelectorAll('.welcome-text');
+  welcomes.forEach(function(welcome,index){
+    setTimeout(() => {
+      welcome.classList.add('second-active');
+    },index*0)
+  });
+};
